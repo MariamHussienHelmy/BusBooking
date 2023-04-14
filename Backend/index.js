2dd3b4065870a84be295fad1f3d726813d133c4e
MariamHussienHelmy <119523474+MariamHussienHelmy@users.noreply.github.com>
2023-04-15 00:47:20 +0200

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+// ==================== INITIALIZE EXPRESS APP ====================
+const express = require("express"); //express عشان نقوم على السيرفر كله
+const app = express(); //call to express
+
+// ====================  GLOBAL MIDDLEWARE ====================
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // TO ACCESS URL FORM ENCODED ==> based on bofy parser to be jason
+app.use(express.static("upload"));
+const cors = require("cors");
+app.use(cors()); // ALLOW HTTP REQUESTS LOCAL HOSTS
+
+// ====================  Required Module ====================
+const auth = require("./routes/Auth");
+//const movies = require("./routes/Movies");
+
+// ====================  RUN THE APP  ====================
+app.listen(8082, "localhost", () => {
+  console.log("SERVER IS RUNNING ");
+});
+
+// ====================  API ROUTES [ ENDPOINTS ]  ====================
+app.use("/auth", auth);
+// app.use("/movies", movies);
