0ad5ae34443249e53699a7d4413e01a3a91cb12d
yara-e <74534446+yara-e@users.noreply.github.com>
2023-04-16 13:40:18 +0200

    Update Header.js

--- Frontend/src/Usercomponents/Header.js
+++ Frontend/src/Usercomponents/Header.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import "../UserStyle/Header.css";
 import { Link } from 'react-router-dom';
+import Axios from "axios";
+export const word = async () => {
+    let status = await Axios.get("http://localhost:8080/auth/isactive");
+    var sign = document.getElementById("sign");
+    let stat = status.data.status + '';
+    if (stat == "active") {
+        if (sign) {
+            sign.innerHTML = "logout";
+        }
+
+    }
+    else if (stat == "inactive") {
+        if (sign) {
+            sign.innerHTML = "login";
+        }
+    }
+    // console.log(sign.innerHTML)
+}
 const Header = () => {
+
+    const logout = async () => {
+        await Axios.put("http://localhost:8080/auth/logout");
+        localStorage.clear();
+        word();
+    };
+
+
+
+    word();
     return (
         <header>
             <div class="inner-width">
@@ -11,7 +39,7 @@ const Header = () => {
                     <Link to="/tickets">Tickets</Link>
                     <Link to="/profile"> Profile </Link>
 
-                    <Link to="/login"> Login</Link>
+                    <Link to="/login" onClick={(logout)} ><div id="sign"> logout</div></Link>
                 </nav>
             </div>
         </header>
