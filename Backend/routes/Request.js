66ee706890059fb0c9743aa44172d8b6cf8237f6
yara-e <74534446+yara-e@users.noreply.github.com>
2023-04-17 13:47:41 +0200

    Update Request.js

--- Backend/routes/Request.js
+++ Backend/routes/Request.js
@@ -62,6 +62,37 @@ router.put(
     }
 )
 
+router.get(
+    "/history/:id",
+    async (req, res) => {
+        const query = util.promisify(conn.query).bind(conn);
+        const appointment = await query("SELECT    appointments.id , appointments.from, appointments.to,appointments.ticket_price, appointments.day_and_time,appointments.max_number_of_travelers FROM appointment_requests join appointments WHERE appointment_requests.traveler_id=? and appointment_requests.id=appointments.id",
+            req.params.id);
 
+        if (!appointment) {
+            res.status(404).json({ ms: "appointment not found !" });
+        }
+        res.status(200).json(appointment);
+    }
+
+)
+
+
+/*SELECT  count(appointment_id) 
+FROM `appointment_requests` 
+WHERE appointment_id=1*/
+
+router.get("/counttravlers/:id",
+    async (req, res) => {
+        const query = util.promisify(conn.query).bind(conn);
+        const number = await query("SELECT  count(appointment_id) FROM appointment_requests  WHERE appointment_id=?", req.params.id);
+        res.status(200).json({
+            number
+        });
+        if (!number) {
+            res.status(404).json({ ms: "no travelers" });
+        }
+    }
+)
 
 module.exports = router;
