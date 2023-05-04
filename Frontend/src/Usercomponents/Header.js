1c7561d78c0e0d689924b0c95aac3a38a51f3fcf
MariamHussienHelmy <maremhelmy43@gmail.com>
2023-05-05 01:02:00 +0200

    Header Updated

--- Frontend/src/Usercomponents/Header.js
+++ Frontend/src/Usercomponents/Header.js
@@ -1,34 +1,44 @@
-import React from "react";
-import "../UserStyle/Header.css";
-import { Link } from "react-router-dom";
-import { removeAuthUser, getAuthUser } from "../helper/Storage";
-import Axios from "axios";
-const Header = () => {
-  const auth = getAuthUser();
-  const logout = async () => {
-    await Axios.put("http://localhost:4000/auth/logout/" + auth.id);
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { removeAuthUser, getAuthUser } from '../helper/Storage';
+import Axios from 'axios';
 
+import '../UserStyle/Header.css';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const auth = getAuthUser();
+  // const Logout = async () => {
+  //   await Axios.put('http://localhost:4000/auth/logout/' + auth.id);
+
+  //   removeAuthUser();
+  // };
+
+  const Logout = () => {
     removeAuthUser();
+    navigate('/');
   };
 
   return (
     <header>
-      <div class="inner-width">
-        <h1 class="logo">Bus Ticket</h1>
-        <i class="menu-toggle-btn fas fa-bars"></i>
-        <nav class="navigation-menu">
-          <Link to="/tickets">Tickets</Link>
-          {auth && <Link to="/profile"> Profile </Link>}
+      <div className='inner-width'>
+        <h1 className='logo'>Bus Ticket</h1>
+        {/* <i class='menu-toggle-btn fas fa-bars'></i> */}
+        <nav className='navigation-menu'>
+          <Link to='/tickets'>Tickets</Link>
+          {auth && <Link to='/profile'>Profile</Link>}
 
           {auth && (
-            <Link to="/login" onClick={logout}>
-              <div id="sign"> logout</div>
+            <Link to='/login' onClick={Logout}>
+              {/* <div id='sign'>Logout</div> */}
+              Logout
             </Link>
           )}
+          {!auth && <Link to='/register'>Signup</Link>}
           {!auth && (
-            <Link to="/login">
-              {" "}
-              <div id="sign"> login</div>
+            <Link to='/login'>
+              Login
+              {/* <div id='sign'>login</div> */}
             </Link>
           )}
         </nav>
