016d2fe3ef11068f0de4c2a3c3923ab85fdd41e4
MariamHussienHelmy <maremhelmy43@gmail.com>
2023-05-05 00:50:17 +0200

    Updated Style

--- Frontend/src/shared/login.js
+++ Frontend/src/shared/login.js
@@ -1,97 +1,133 @@
-import React from "react";
-import "./login.css";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
-import Axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { setAuthUser, getAuthUser } from "../helper/Storage";
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import Axios from 'axios';
+
+import Header from '../Usercomponents/Header';
+import { setAuthUser, getAuthUser } from '../helper/Storage';
+
+import classes from './login.module.css';
+
 function Login() {
-  const auth = getAuthUser();
-  const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm();
+	const auth = getAuthUser();
+	const navigate = useNavigate();
 
-  const onSubmit = async (data) => {
-    let result = await Axios.post("http://localhost:4000/auth/login", {
-      // data
-      email: data.email,
-      password: data.password,
-    })
-      .then((resp) => {
-        setAuthUser(resp.data);
-        if (auth && auth.type == "admin") {
-          navigate("/manageappoint");
-        } else if (auth && auth.type == "traveler") {
-          navigate("/tickets");
-        }
-      })
-      .catch((e) => {
-        const errors = e.response.data;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-        if (errors.err) {
-          setError("email", {
-            type: "server",
-            // message: "wrong email"
-          });
-        }
-        if (errors.errors[0].msg) {
-          setError("password", {
-            type: "server",
-            message: errors.errors[0].msg,
-          });
-        }
-      });
-  };
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors },
+	} = useForm();
 
-  return (
-    <div className="wrapper">
-      {errors.email && (
-        <p className="error">
-          {errors.email.message} something went wrong with email
-        </p>
-      )}
-      {errors.password && (
-        <p className="error">
-          {errors.password.message} password at least 8 characters
-        </p>
-      )}
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="input-box">
-          <input
-            type="text"
-            placeholder="Enter your email"
-            required
-            {...register("email", {
-              pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
-            })}
-          />
-        </div>
-        <div className="input-box">
-          <input
-            type="password"
-            placeholder="password"
-            required
-            {...register("password", {
-              minLength: 8,
-            })}
-          />
-        </div>
+	const emailChangeHandler = event => {
+		setEmail(event.target.value);
+	};
 
-        <div className="input-box button">
-          <input type="Submit" value="Login" />
-        </div>
-        <div className="text">
-          <h3>
-            Don't have an account? <Link to="/register">Register now</Link>
-          </h3>
-        </div>
-      </form>
-    </div>
-  );
+	const passwordChangeHandler = event => {
+		setPassword(event.target.value);
+	};
+
+	const submitHandler = async event => {
+		// Prevent reload after form submission
+		event.preventDefault();
+
+		const data = { email, password };
+
+		try {
+			const result = await Axios.post('http://localhost:4000/auth/login', data);
+
+			console.log(result.data.type);
+			// console.log(result.data);
+			setAuthUser(result.data);
+			console.log(auth);
+			if (result.data.type == 'admin') {
+				console.log('it should navigate to ManageAppointments');
+				navigate('/manageappoint');
+			} else {
+				navigate('/tickets');
+			}
+		} catch (err) {
+			const errors = err.response.data;
+
+			if (errors.err) {
+				setError('email', {
+					type: 'server',
+					// message: "wrong email"
+				});
+			}
+			if (errors.errors[0].msg) {
+				setError('password', {
+					type: 'server',
+					message: errors.errors[0].msg,
+				});
+			}
+		}
+	};
+
+	return (
+		<>
+			<Header />
+			<div className={classes.wrapper}>
+				{errors.email && (
+					<p className={classes.error}>
+						{errors.email.message} Something went wrong with email
+					</p>
+				)}
+				{errors.password && (
+					<p className={classes.error}>
+						{errors.password.message} password at least 8 characters
+					</p>
+				)}
+				<h2>Login</h2>
+				<form onSubmit={submitHandler}>
+					<div className={classes['input-box']}>
+						<label htmlFor='email'>Email</label>
+						<input
+							id='email'
+							name='email'
+							type='text'
+							// placeholder='Enter your email'
+							required
+							{...register('email', {
+								pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+							})}
+							value={email}
+							onChange={emailChangeHandler}
+						/>
+					</div>
+					<div className={classes['input-box']}>
+						<label htmlFor='password'>Password</label>
+						<input
+							id='password'
+							name='password'
+							type='password'
+							// placeholder='Password'
+							required
+							{...register('password', {
+								minLength: 8,
+							})}
+							value={password}
+							onChange={passwordChangeHandler}
+						/>
+					</div>
+
+					<div className={classes['input-box']}>
+						<button className={classes['btn--login']} type='Submit'>
+							Login
+						</button>
+					</div>
+					<div className={classes.text}>
+						<h3>
+							Don't have an account? <br />{' '}
+							<Link to='/register'>Register Now</Link>
+						</h3>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 }
 export default Login;
--- Frontend/src/shared/register.js
+++ Frontend/src/shared/register.js
@@ -1,102 +1,154 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import "./register.css";
-import { Link } from "react-router-dom";
-import Axios from "axios";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useForm } from 'react-hook-form';
+import Axios from 'axios';
+
+import Header from '../Usercomponents/Header';
+
+import classes from './register.module.css';
 
 const Register = () => {
-  const [error, setError] = useState("");
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+	const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    Axios.post("http://localhost:4000/auth/register", {
-      // data
-      name: data.name,
-      email: data.email,
-      phone: data.phone,
-      password: data.password,
-    })
-      .then((response) => {
-        // handle success response
-        console.log(response.data);
-      })
-      .catch((error) => {
-        // handle error response
-        if (error.response) {
-          setError(error.response.data.errors[0].msg);
-          //alert(error.response.data.errors[0].msg);
-        } else {
-          setError("Something went wrong. Please try again later.");
-        }
-      });
-  };
+	const [error, setError] = useState('');
 
-  return (
-    <div className="wrapper">
-      {error && <h1 className="error">{error}</h1>}
-      <h2>Registration</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="input-box">
-          <input
-            type="text"
-            placeholder="Enter your name"
-            {...register("name", { required: true })}
-            required
-          />
-          {errors.name && <p className="error">enter your name</p>}
-        </div>
-        <div className="input-box">
-          <input
-            type="text"
-            placeholder="Enter your phone"
-            {...register("phone", { required: true })}
-            required
-          />
-          {errors.name && <p className="error">enter your name</p>}
-        </div>
-        <div className="input-box">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
-            required
-          />
-          {errors.email?.type === "required" && (
-            <p className="error">enter your email</p>
-          )}
-          {errors.email?.type === "pattern" && (
-            <p className="error">enter a valid email</p>
-          )}
-        </div>
-        <div className="input-box">
-          <input
-            type="password"
-            placeholder="Create password"
-            {...register("password", { required: true, minLength: 8 })}
-            required
-          />
-          {errors.password?.type === "required" && (
-            <p className="error">enter your password</p>
-          )}
-          {errors.password?.type === "minLength" && (
-            <p className="error">Password at least 8 characters</p>
-          )}
-        </div>
-        <div className="input-box button">
-          <input type="submit" value="Register Now" />
-        </div>
-        <div className="text">
-          <h3>
-            Already have an account? <Link to="/login">Login now</Link>
-          </h3>
-        </div>
-      </form>
-    </div>
-  );
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [phone, setPhone] = useState('');
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const usernameChangeHandler = event => {
+		setName(event.target.value);
+	};
+
+	const phoneChangeHandler = event => {
+		setPhone(event.target.value);
+	};
+
+	const passwordChangeHandler = event => {
+		setPassword(event.target.value);
+	};
+
+	const emailChangeHandler = event => {
+		setEmail(event.target.value);
+	};
+
+	const submitHandler = async event => {
+		// Prevent reload after form submission
+		event.preventDefault();
+
+		const data = { name, phone, email, password };
+		try {
+			const response = await Axios.post(
+				'http://localhost:4000/auth/register',
+				data
+			);
+
+			console.log(response);
+			// console.log('submitted');
+
+			navigate('/login');
+		} catch (error) {
+			// handle error response
+			console.log(error);
+			if (error.response) {
+				setError(error.response.data.errors[0].msg);
+				//alert(error.response.data.errors[0].msg);
+			} else {
+				setError('Something went wrong. Please try again later.');
+			}
+		}
+	};
+
+	return (
+		<>
+			<Header />
+			<div className={classes.wrapper}>
+				{error && <h4 className={classes.error}>{error}</h4>}
+				<h2>Registration</h2>
+				<form onSubmit={submitHandler}>
+					<div className={classes['input-box']}>
+						<label htmlFor='name'>Name</label>
+						<input
+							id='name'
+							name='name'
+							type='text'
+							required
+							value={name}
+							onChange={usernameChangeHandler}
+						/>
+						{errors.name && <p className={classes.error}>Enter your name</p>}
+					</div>
+					<div className={classes['input-box']}>
+						<label htmlFor='phone'>Phone</label>
+						<input
+							id='phone'
+							name='phone'
+							type='text'
+							required
+							value={phone}
+							onChange={phoneChangeHandler}
+						/>
+						{errors.name && <p className={classes.error}>Enter your phone</p>}
+					</div>
+					<div className={classes['input-box']}>
+						<label htmlFor='email'>Email</label>
+						<input
+							id='email'
+							name='email'
+							type='email'
+							required
+							value={email}
+							onChange={emailChangeHandler}
+						/>
+						{errors.email?.type === 'required' && (
+							<p className='error'>enter your email</p>
+						)}
+						{errors.email?.type === 'pattern' && (
+							<p className='error'>enter a valid email</p>
+						)}
+					</div>
+					<div className={classes['input-box']}>
+						<label htmlFor='password'>Password</label>
+						<input
+							id='password'
+							name='password'
+							type='password'
+							// {...register('password', { required: true, minLength: 8 })}
+							required
+							value={password}
+							onChange={passwordChangeHandler}
+						/>
+						{errors.password?.type === 'required' && (
+							<p className={classes.error}>Enter your password</p>
+						)}
+						{errors.password?.type === 'minLength' && (
+							<p className={classes.error}>
+								Password must be at least 8 characters
+							</p>
+						)}
+					</div>
+					<div className={classes['input-box']}>
+						<button className={classes['btn--submit']} type='submit'>
+							Register Now
+						</button>
+					</div>
+					<div className={classes.text}>
+						<h3>
+							Already have an account? <Link to='/login'>Login now</Link>
+						</h3>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Register;
